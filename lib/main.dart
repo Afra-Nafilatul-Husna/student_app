0937da12a5a7a561faffd532b5feebff1bc8c5ef
19231754_AFRA_NAFILATUL_HUSNA <19231754@bsi.ac.id>
2025-05-04 12:14:55 +0000

    main

--- lib/main.dart
+++ lib/main.dart
@@ -1,43 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:latihan_flutter/column_widget.dart';
+import 'package:latihan_flutter/contoh_stateful.dart';
+import 'package:latihan_flutter/contoh_stateless.dart';
+import 'package:latihan_flutter/halaman_kedua.dart';
+import 'package:latihan_flutter/halaman_pertama.dart';
+import 'package:latihan_flutter/hello_world.dart';
+import 'package:latihan_flutter/mahasiswa_form.dart';
+import 'package:latihan_flutter/parsing.dart';
+import 'package:latihan_flutter/row_widget.dart';
 
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
-  // This widget is the root of your application.
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
-      ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
-        ),
-      ),
+      title: 'Latihan Flutter',
+      home: MahasiswaForm(),
     );
   }
 }
